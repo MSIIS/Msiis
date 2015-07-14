d2c2fdfd839811e904ebe187e1c73661f9824728
guozhihua <237803546@qq.com>
2015-07-14 17:45:30 +0800

    viwe

--- web/src/main/java/com/web/controller/view/UserRegesterController.java
+++ web/src/main/java/com/web/controller/view/UserRegesterController.java
@@ -9,16 +9,13 @@ import com.web.soupe.dto.SoupeWebModel;
 import com.web.soupe.web.Organization;
 import com.web.soupe.web.User;
 import org.apache.log4j.Logger;
-import org.apache.log4j.spi.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Date;
 import java.util.List;
 
 /**
