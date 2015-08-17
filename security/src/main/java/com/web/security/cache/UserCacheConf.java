b3831febd167215e24d9fa29c4cf8bbf04b9d598
guozhihua <237803546@qq.com>
2015-08-17 14:10:54 +0800

    定义用户缓存名

--- security/src/main/java/com/web/security/cache/UserCacheConf.java
+++ security/src/main/java/com/web/security/cache/UserCacheConf.java
@@ -4,7 +4,7 @@ package com.web.security.cache;
  * Created by nlf on 2015-7-13.
  */
 public class UserCacheConf {
-    public  static final String USER_NAME="USER_ANME_";
+    public  static final String USER_NAME="USER_NAME_";
 
 
 }
